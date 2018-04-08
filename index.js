6089091a37588062a45bd689b22a2ffb72cc7a97
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-08 22:40:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ function logWhisper(string) {
   
 }
 
-// (string) takes a string as input and console logs said
-// string back to the user in all lowercase.
+// sayHiToGrandma(string) takes a string as input and returns
+// a specific response based on whether the input string is
+// lowercase, uppercase, or "I love you, Grandma."
 function sayHiToGrandma(string) {
   
 }
