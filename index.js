3819d92e9072bcac13f31cbb4a84f60991d1f869
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-08 22:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ function logWhisper(string) {
 // lowercase, uppercase, or "I love you, Grandma."
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
-    console.log("I can't hear you!");
+    return "I can't hear you!";
   }
   else if (string === shout(string)) {
-    console.log("YES INDEED!");
+    return "YES INDEED!";
   }
   else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.");
+    return "I love you, too.";
   }
 }
 
