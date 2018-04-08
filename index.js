1e06cf9c459115c1d4bbdeb17fcdaa2a4e6d85f4
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-08 22:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Intro to Functions Lab
+// Laura Nadolski
+// 4/8/18
+
+// shout(string) takes a string as input and returns said
+// string back to the user in all caps.
 function shout (string) {
+  
+}
+
+function whisper (string) {
   
 }
